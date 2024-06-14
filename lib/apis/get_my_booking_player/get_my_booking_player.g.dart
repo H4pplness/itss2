ee0a238e9b745c4d784b599d81ed566c51e1bb5d
Sukun444 <vanhein445566@gmail.com>
2024-06-14 15:10:31 +0700

    first commit

---
+++ lib/apis/get_my_booking_player/get_my_booking_player.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_my_booking_player.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getMyBookingPlayerHash() =>
+    r'c559faa734208d46ae6fc2d2233bf76c131138aa';
+
+/// See also [getMyBookingPlayer].
+@ProviderFor(getMyBookingPlayer)
+final getMyBookingPlayerProvider =
+    AutoDisposeFutureProvider<List<GetMyBookingPlayerDTO>>.internal(
+  getMyBookingPlayer,
+  name: r'getMyBookingPlayerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getMyBookingPlayerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetMyBookingPlayerRef
+    = AutoDisposeFutureProviderRef<List<GetMyBookingPlayerDTO>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
